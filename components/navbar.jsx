901255da874bac7725880683d70f4f823776308d
Anubhab Nath <152593435+anubhabnath098@users.noreply.github.com>
2024-03-24 06:32:25 +0000

    Initial commit

---
+++ components/navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../public/styles.css";
+function Navbar() {
+  const navStyle = {
+    display: "flex",
+    listStyle: "none",
+    gap: "20px",
+    height: "10vh",
+    backgroundColor: "purple",
+    alignItems: "center",
+    paddingLeft: "1rem",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    zIndex: "100",
+  };
+
+  return (
+    <div className="navbar" style={navStyle}>
+      <li>
+        <NavLink
+          style={({ isActive, isPending }) => {
+            return {
+              color: isActive ? "red" : "white",
+              textDecoration: isActive ? "none" : "none",
+            };
+          }}
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive, isPending }) => {
+            return {
+              color: isActive ? "red" : "white",
+              textDecoration: isActive ? "none" : "none",
+            };
+          }}
+          to="/about"
+        >
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive, isPending }) => {
+            return {
+              color: isActive ? "red" : "white",
+              textDecoration: isActive ? "none" : "none",
+            };
+          }}
+          to="/contact"
+        >
+          Contact
+        </NavLink>
+      </li>
+    </div>
+  );
+}
+
+export default Navbar;
